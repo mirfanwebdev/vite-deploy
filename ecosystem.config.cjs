e7c698143edbae339121cfa39943ba9dd1a6bd40
m.irfan <m.irfanwebdev@gmail.com>
2024-01-27 10:53:29 +0700

    fix config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "vite-deploy",
-      script: "start",
+      script: "serve -s build -p 5173",
       error_file: "./pm2-error.log",
       out_file: "./pm2-out.log",
       env: {
