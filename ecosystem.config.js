a70e288422889b98155206a2312575c98f4971f2
m.irfan <m.irfanwebdev@gmail.com>
2024-01-27 13:09:09 +0700

    new config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: "vite-deploy",
+      name: `vite-deploy`,
       script: "serve",
-      args: "-s build",
-      error_file: "./pm2-error.log",
-      out_file: "./pm2-out.log",
       env: {
+        PM2_SERVE_PATH: "./dist",
+        PM2_SERVE_PORT: 5173,
+        PM2_SERVE_SPA: "true",
         NODE_ENV: "production",
       },
     },
